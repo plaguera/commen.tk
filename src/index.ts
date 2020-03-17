61fae65e92c5a4dc45b67f6793d6f8038d49ec79
Pedro Lagüera <laguerapedro@gmail.com>
2020-03-17 21:54:36 +0000

    Index file is set

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,9 @@
 import { Server } from './server'
-import { Auth } from './auth';
-import { OAuth } from './oauth';
 
 require('dotenv').config();
-Auth.validate();
 
 let port = process.env.PORT || '3040';
 let server = new Server();
 server.socket.listen(port, function () {
     console.log(`Listening on port ${port}`);
 });
-
-process.on('SIGINT', () => { console.log("Bye bye!"); process.exit(); })
