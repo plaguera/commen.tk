57acdfbe0db19a6509a295938f647b74050b99bd
Pedro Lagüera <laguerapedro@gmail.com>
2020-03-29 20:36:52 +0100

    Removed outdated server code

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -3,7 +3,7 @@ export class Controller {
 	static token: string;
 
 	static getToken(req: Request, res: Response, next) {
-        console.log(req.headers.authorization);
+        //console.log(req.headers.authorization);
 		if (req.headers.authorization)
 			Controller.token = req.headers.authorization;
 		next();
