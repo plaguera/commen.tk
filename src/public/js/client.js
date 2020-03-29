f9c8c6b8e56500c2148c0f386d7a099aa63d462b
Pedro Lagüera <laguerapedro@gmail.com>
2020-03-29 20:35:29 +0100

    Removed JS widget code

--- src/public/js/client.js
+++
@@ -1,6 +0,0 @@
-import '../scss/client.scss';
-import { Auth } from './auth';
-import { CommentWidget } from "./comment-widget"
-
-Auth.init();
-CommentWidget.init();
--- src/public/js/index.js
+++
@@ -1,9 +0,0 @@
-import '../scss/client.scss';
-import '../html/index.html';
-import { Auth } from "./auth";
-import { CommentWidget } from "./comment-widget";
-import { Header } from "./header";
-
-Auth.init();
-document.body.before(new Header().element);
-CommentWidget.init();
