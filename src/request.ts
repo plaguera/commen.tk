86dfed156248da0f3d5780820d888cb2b4eee875
Pedro Lagüera <laguerapedro@gmail.com>
2020-04-04 12:35:57 +0100

    heroku

--- src/request.ts
+++ src/request.ts
@@ -48,6 +48,7 @@ export async function query(data: string) {
 
 	let res = await fetch('https://api.github.com/graphql', options);
 	let json = await res.json();
+	console.log(json);
 	return {
 		url: res.url,
 		status: res.status,
