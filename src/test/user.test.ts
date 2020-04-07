9d2ed0fdcde68b692252ca3c1d32c5fb72695c1f
Pedro Lagüera <laguerapedro@gmail.com>
2020-04-07 22:52:06 +0100

    Fixed Tests

--- src/test/user.test.ts
+++ src/test/user.test.ts
@@ -7,22 +7,25 @@ chai.use(chaiHttp);
 
 describe('Server!', () => {
 	var server = require('../index');
-	after(function() {
+	after(function () {
 		server.close();
 	});
-	it("Searches for a user 'plaguera'", done => {
+	it("Searches for a user 'plaguera'", (done) => {
 		chai.request(server)
 			.get('/api/users/plaguera')
 			.end((err, res) => {
 				expect(res).to.have.status(200);
-				expect(res.body).to.have.property('data');
-				expect(res.body.data).to.have.property('user');
-				expect(res.body.data.user).to.have.property('login');
-				expect(res.body.data.user).to.have.property('url');
-        expect(res.body.data.user).to.have.property('avatarUrl');
-        expect(res.body.data.user.login).to.equals('plaguera');
-        expect(res.body.data.user.url).to.equals('https://github.com/plaguera');
-        expect(res.body.data.user.avatarUrl).to.equals('https://avatars2.githubusercontent.com/u/22492917?u=cd106dc1a9d6a362cbf74a36a6268ede717bdc04&v=4');
+				expect(res.body).to.have.property('user');
+				expect(res.body.user).to.have.property('login');
+				expect(res.body.user).to.have.property('url');
+				expect(res.body.user).to.have.property('avatarUrl');
+				expect(res.body.user.login).to.equals('plaguera');
+				expect(res.body.user.url).to.equals(
+					'https://github.com/plaguera'
+				);
+				expect(res.body.user.avatarUrl).to.equals(
+					'https://avatars2.githubusercontent.com/u/22492917?u=cd106dc1a9d6a362cbf74a36a6268ede717bdc04&v=4'
+				);
 				done();
 			});
 	});
