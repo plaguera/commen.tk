36fb63081c885f9ef41d92aa91489efb766c0681
Pedro Lagüera <laguerapedro@gmail.com>
2020-04-26 00:13:14 +0100

    Stylesheet is now inside <head>

--- frontend/src/Widget.tsx
+++ frontend/src/Widget.tsx
@@ -5,7 +5,6 @@ import Header from './components/Header';
 import Timeline from './components/Timeline';
 import { WidgetProps, WidgetState } from './props';
 import PaginationButton from './components/PaginationButton';
-import { PageAttributes } from './page-attributes';
 
 class Widget extends React.Component<WidgetProps, WidgetState> {
 
@@ -77,7 +76,6 @@ class Widget extends React.Component<WidgetProps, WidgetState> {
 	render() {
 		return (
 			<div className='widget-wrapper'>
-				<link rel='stylesheet' type='text/css' href={PageAttributes.base_url + 'themes/' + this.props.theme + '.css'} />
 				<Header commentCount={this.state.totalCount} url={this.issueUrl()} />
 				<div className='timeline-wrapper'>
 					<PaginationButton hiddenItems={this.state.hiddenItems} onClick={this.nextComments.bind(this)} user={this.state.user}/>
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,12 +4,25 @@ import Widget from './Widget';
 import * as serviceWorker from './serviceWorker';
 import * as attributes from './page-attributes';
 
+function addCss(theme: string) {
+	var head = document.head;
+	var link = document.createElement("link");
+
+	link.type = "text/css";
+	link.rel = "stylesheet";
+	link.href = attributes.PageAttributes.base_url + 'themes/' + theme + '.css';
+
+	head.appendChild(link);
+}
+
 console.log(process.env.NODE_ENV);
 let script = document.currentScript;
 if (script && script.parentElement) {
 	var tmp = document.createElement('div');
 	tmp.className = 'github-comments';
-	ReactDOM.render(<Widget {...attributes.parse(script)} />, tmp);
+	let attrs = attributes.parse(script);
+	ReactDOM.render(<Widget {...attrs} />, tmp);
+	addCss(attrs.theme);
 	script.parentElement.replaceChild(tmp, script);
 }
 
