c642ec0cadc8c6b94228b5fe387892a84ff05dfe
Pedro Lagüera <laguerapedro@gmail.com>
2020-04-28 17:12:38 +0100

    Added api root html message

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,11 +5,12 @@ import { AuthController } from '../controllers/auth-controller';
 
 const routes = Router();
 
-routes.route("/api/user").get(UserController.get)
-routes.route("/api/users/:id").get(UserController.get);
-routes.route("/api/comments/:user/:repo/:issuenumber").get(IssueController.get);
-routes.route("/api/comments/:user/:repo/:issuenumber").post(IssueController.post);
-routes.route("/authorize").get(AuthController.authorize);
-routes.route("/oauth/redirect").get(AuthController.access_token);
+routes.route('/').get((req, res) => res.send('<h1>API Server is </h1><h1 style="color: green;">RUNNING</h1>'));
+routes.route('/api/user').get(UserController.get)
+routes.route('/api/users/:id').get(UserController.get);
+routes.route('/api/comments/:user/:repo/:issuenumber').get(IssueController.get);
+routes.route('/api/comments/:user/:repo/:issuenumber').post(IssueController.post);
+routes.route('/authorize').get(AuthController.authorize);
+routes.route('/oauth/redirect').get(AuthController.access_token);
 
 export default routes;
