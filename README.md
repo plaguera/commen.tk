6869744c30b7f51eb059c2f01f610d8bebf5b806
Pedro Lagüera <laguerapedro@gmail.com>
2020-07-05 11:01:48 +0100

    Changed API root html meesage

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 [![Build Status](https://travis-ci.com/plaguera/commen.tk.svg?branch=master)](https://travis-ci.com/plaguera/commen.tk)
 [![codecov](https://codecov.io/gh/plaguera/commen.tk/branch/master/graph/badge.svg)](https://codecov.io/gh/plaguera/commen.tk)
 [![Known Vulnerabilities](https://snyk.io/test/github/plaguera/commen.tk/badge.svg?targetFile=package.json)](https://snyk.io/test/github/plaguera/commen.tk?targetFile=package.json)
-![Website](https://img.shields.io/website?url=https%3A%2F%commen.tk)
+![Website](https://img.shields.io/website?url=https%3A%2F%2Fcommen.tk)
 ![API](https://img.shields.io/website?url=https%3A%2F%2Fapi.commen.tk)
 ![CDN](https://img.shields.io/website?url=https%3A%2F%2Fcdn.commen.tk)
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,7 +6,9 @@ import { CommentController } from '../controllers/comment-controller';
 
 const routes = Router();
 
-routes.route('/').get((req, res) => res.status(200).send('<h1>API Server is <strong style="color: green;">RUNNING</strong></h1>'));
+const indexHTML = '<!DOCTYPE html><html lang="en"><head><meta charset="utf-8" /><title>API commen.tk</title><meta name="description" content="API for commen.tk" /><meta name="author" content="plaguera" /></head><body><h1 style="font-family: Arial, Helvetica, sans-serif;">API is <strong style="color: green;">UP</strong></h1></body></html>';
+
+routes.route('/').get((req, res) => res.status(200).send(indexHTML));
 routes.route('/user').get(UserController.get)
 routes.route('/users/:id').get(UserController.get);
 routes.route('/issuenumber/:owner/:repo/:name').get(IssueController.processIssueName);
