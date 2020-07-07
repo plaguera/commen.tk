8326a55e2d12b18b4886b32d933083a232c70a65
Pedro Lagüera <laguerapedro@gmail.com>
2020-07-07 18:45:25 +0100

    Added try catch block in for script attribute errors

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -5,16 +5,20 @@ import * as serviceWorker from './serviceWorker';
 import { loadTheme, ScriptAttributes } from './util';
 import env from './environment';
 
-console.log(env.node_env);
-let script = document.currentScript;
-if (script && script.parentElement) {
-	var tmp = document.createElement('div');
-	tmp.className = 'commen.tk';
-	let attrs = new ScriptAttributes(script).toJSON();
-	loadTheme(attrs.theme).then(() => {
-		ReactDOM.render(<Widget {...attrs} />, tmp);
-		if (script && script.parentElement) script.parentElement.replaceChild(tmp, script);
-	});
+try {
+	console.log(env.node_env);
+	let script = document.currentScript;
+	if (script && script.parentElement) {
+		var tmp = document.createElement('div');
+		tmp.className = 'commen.tk';
+		let attrs = new ScriptAttributes(script).toJSON();
+		loadTheme(attrs.theme).then(() => {
+			ReactDOM.render(<Widget {...attrs} />, tmp);
+			if (script && script.parentElement) script.parentElement.replaceChild(tmp, script);
+		});
+	}
+} catch(e) {
+	console.error(e);
 }
 
 // If you want your app to work offline and load faster, you can change
