98af38d676518ff5f541cfac2b42631b96ede412
Pedro Lagüera <laguerapedro@gmail.com>
2020-07-07 08:53:04 +0100

    Documented: Controller

--- src/controllers/controller.ts
+++ src/controllers/controller.ts
@@ -2,11 +2,27 @@ import { Request, Response, CookieOptions } from 'express';
 import { InstallationController } from './installation-controller';
 import log from '../logger';
 
+/**
+ * Base class for server controllers
+ */
 export class Controller {
+	/**
+	 * Send @res with status code @status and payload @data
+	 * @param res Response
+	 * @param status Status Code
+	 * @param data Payload
+	 */
 	static sendResponse(res: Response, status: number, data: any) {
 		res.status(status).send(data);
 	}
 
+	/**
+	 * Check whether @req has an access token cookie or and installation access token cookie.
+	 * Check for authorization header, used only in testing.
+	 * If none of the above exist, a new installation access token cookie is created and sent with @res
+	 * @param req Request
+	 * @param res Response
+	 */
 	static async token(req: Request, res: Response) {
 		if (req.signedCookies.token) {
 			return req.signedCookies.token;
@@ -19,6 +35,16 @@ export class Controller {
 		}
 	}
 
+	/**
+	 * Check if @name cookie exists in @req, if it does, do nothing.
+	 * If it doesn't create @name cookie with @value and @maxAge in @res and send it.
+	 * Development builds don't use HTTPS, thus cookies can't have the @secure attribute, without @secure, 'Samesite: none' can't be used.
+	 * @param req Request
+	 * @param res Response
+	 * @param name Cookie Name
+	 * @param value Cookie Value
+	 * @param maxAge Cookie Time to Live in milliseconds
+	 */
 	static checkCookie(req: Request, res: Response, name: string, value: string, maxAge: number) {
 		if (process.env.NODE_ENV === 'PRODUCTION') {
 			let cookie = req.signedCookies[name];
@@ -36,7 +62,6 @@ export class Controller {
 		} else if (process.env.NODE_ENV === 'DEVELOPMENT') {
 			let cookie = req.signedCookies[name];
 			if (cookie === undefined) {
-				
 				let options : CookieOptions = {
 					httpOnly: true,
 					maxAge: maxAge,
