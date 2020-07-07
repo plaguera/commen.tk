1b532c4f294e1202825923bbbd19646a1418633e
Pedro Lagüera <laguerapedro@gmail.com>
2020-07-07 10:27:32 +0100

    Created @octokit/graphql function

---
+++ src/environment.ts
@@ -0,0 +1,27 @@
+const env = {
+	cwd: process.cwd(),
+	development: process.env.NODE_ENV?.toLowerCase() === 'development',
+	production: process.env.NODE_ENV?.toLowerCase() === 'production',
+	test: process.env.NODE_ENV?.toLowerCase() === 'test',
+	node_env: process.env.NODE_ENV,
+	oauth: {
+		client_id: process.env[`${process.env.NODE_ENV}_CLIENT_ID`],
+		client_secret: process.env[`${process.env.NODE_ENV}_CLIENT_SECRET`],
+		url_authorize: 'https://github.com/login/oauth/authorize',
+		url_access_token: 'https://github.com/login/oauth/access_token',
+	},
+	github_app: {
+		identifier: process.env.GITHUB_APP_IDENTIFIER,
+		private_key_path: process.env.GITHUB_APP_PRIVATE_KEY_PATH
+	},
+	page_size: {
+		min: 1,
+		max: 100,
+		default: 10
+	},
+	port: process.env.PORT || '8000',
+	url_api: 'https://api.github.com/',
+	url_graphql: 'https://api.github.com/graphql'
+};
+
+export default env;
--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,52 @@ import express from './server'
 import fs from 'fs';
 import https from 'https';
 import log from './logger';
+import { graphql } from '@octokit/graphql';
+
+async function aux() {
+	let query = {
+		query: `query RepoID ($repo: String!, $owner: String!) {
+            repository(name: $repo, owner: $owner) {
+				id
+            }
+        }`,
+		owner: 'plaguera',
+		repo: 'tfm-testing',
+		headers: {
+			authorization: `token ${process.env.PORT}`
+		}
+	};
+	try {
+		const { repository } = await graphql(query);
+		return repository;
+	} catch (error) {
+		console.log('Request failed:', error);
+	}
+}
+
+aux().then(console.log);
+
 
 log.debug('NODE_ENV', process.env.NODE_ENV);
 log.debug('CWD\t', process.cwd());
 let port = process.env.PORT || '8000';
+/*
 switch (process.env.NODE_ENV) {
     case 'DEVELOPMENT': httpServer(); break;
     case 'PRODUCTION': httpsServer(); break;
-}
+}*/
 
 /**
  * Creates a HTTPS server using a domain private key and  certificate on port @port
  * Used in production.
  */
 function httpsServer() {
-    https.createServer({
-        key: fs.readFileSync('/etc/letsencrypt/live/api.commen.tk/privkey.pem'),
-        cert: fs.readFileSync('/etc/letsencrypt/live/api.commen.tk/cert.pem'),
-    }, express).listen(port, () => {
-        console.log(`Server started on port ${port}`);
-    });
+	https.createServer({
+		key: fs.readFileSync('/etc/letsencrypt/live/api.commen.tk/privkey.pem'),
+		cert: fs.readFileSync('/etc/letsencrypt/live/api.commen.tk/cert.pem'),
+	}, express).listen(port, () => {
+		console.log(`Server started on port ${port}`);
+	});
 }
 
 /**
@@ -30,7 +56,7 @@ function httpsServer() {
  * Used in only in development and testing environments.
  */
 function httpServer() {
-    express.listen(port, () => {
-        console.log(`Server started on port ${port}`);
-    });
+	express.listen(port, () => {
+		console.log(`Server started on port ${port}`);
+	});
 }
