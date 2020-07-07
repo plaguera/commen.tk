af55ab18dc6dd5b4f3ea5191666ef94a34e60757
Pedro Lagüera <laguerapedro@gmail.com>
2020-07-07 22:08:57 +0100

    Repository Id Caching

--- src/controllers/repository-controller.ts
+++ src/controllers/repository-controller.ts
@@ -6,6 +6,7 @@ import { Request, Response } from 'express';
  * Controller in charge of repository requests.
  */
 export class RepositoryController extends Controller {
+	static repoIds = {};
 	/**
 	 * Returns repository id.
 	 * @param owner Repository Owner
@@ -13,6 +14,8 @@ export class RepositoryController extends Controller {
 	 * @param token Access Token
 	 */
 	static async get(req: Request, res: Response) {
+		if (RepositoryController.repoIds[`${req.params.owner}/${req.params.repo}`])
+			return RepositoryController.repoIds[`${req.params.owner}/${req.params.repo}`];
 		let query: RequestParameters = {
 			query: `query repoID ($repo: String!, $owner: String!) {
 				repository(name: $repo, owner: $owner) {
@@ -23,6 +26,7 @@ export class RepositoryController extends Controller {
 			repo: req.params.repo
 		};
 		const result = await Controller.graphql(req, res, query, true);
+		RepositoryController.repoIds[`${req.params.owner}/${req.params.repo}`] = result.repository.id;
 		return result.repository.id;
 	}
 }
