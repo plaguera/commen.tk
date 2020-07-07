79db5db85febefcc5385a13fdbea93734c28adb8
Pedro Lagüera <laguerapedro@gmail.com>
2020-07-07 22:55:49 +0100

    Basic Options Button

--- frontend/src/components/Editor.tsx
+++ frontend/src/components/Editor.tsx
@@ -70,6 +70,9 @@ class Editor extends React.Component<EditorProps, EditorState> {
                             <TabList>
                                 <Tab>Write</Tab>
                                 <Tab disabled>Preview</Tab>
+                                <div className='btn-options'>
+                                <svg aria-label="Show options" className="octicon octicon-kebab-horizontal" viewBox="0 0 16 16" version="1.1" width="16" height="16" role="img"><path d="M8 9a1.5 1.5 0 100-3 1.5 1.5 0 000 3zM1.5 9a1.5 1.5 0 100-3 1.5 1.5 0 000 3zm13 0a1.5 1.5 0 100-3 1.5 1.5 0 000 3z"></path></svg>
+                                </div>
                             </TabList>
 
                             <TabPanel>
