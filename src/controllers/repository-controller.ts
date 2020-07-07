276c8f63ff40d7428813d7eb54e498a93e0914e5
Pedro Lagüera <laguerapedro@gmail.com>
2020-07-07 08:55:48 +0100

    Documented: Repository Controller

--- src/controllers/repository-controller.ts
+++ src/controllers/repository-controller.ts
@@ -1,7 +1,16 @@
 import { Controller } from './controller';
 import { query } from '../request';
 
+/**
+ * Controller in charge of repository requests.
+ */
 export class RepositoryController extends Controller {
+	/**
+	 * Returns repository id.
+	 * @param owner Repository Owner
+	 * @param repo Repository Name
+	 * @param token Access Token
+	 */
 	static get(owner: string, repo: string, token:string) {
 		let data = `{
             repository(name: "${repo}", owner: "${owner}") {
