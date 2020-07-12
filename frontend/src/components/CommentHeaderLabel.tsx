e3ff5d0cb1e7712fa2613177b707dbeac486bebc
Pedro Lagüera <laguerapedro@gmail.com>
2020-07-12 09:15:17 +0100

    Added comment header label tooltip

--- frontend/src/components/CommentHeaderLabel.tsx
+++ frontend/src/components/CommentHeaderLabel.tsx
@@ -1,14 +1,16 @@
 import * as React from 'react';
 import { CommentHeaderLabelProps } from '../props';
+import env from '../environment';
 
 class CommentHeaderLabel extends React.Component<CommentHeaderLabelProps, {}> {
     render() {
         let assoc = this.props.authorAssociation;
         if (assoc !== 'NONE') {
-            let className = AUTHOR_ASSOCIATION_STRONG[assoc] ? 'comment-header-label strong' : 'comment-header-label';
+            let className = AUTHOR_ASSOCIATION_STRONG[assoc] ? 'comment-header-label strong tooltip' : 'comment-header-label tooltip';
             let text = this.props.authorAssociation.toLocaleLowerCase().replace(/^\w/, c => c.toUpperCase());
+            let tooltip = `You are the ${assoc.toLowerCase()} of the ${env.attributes.repo.split('/')[1]} repository`;
             return (
-                <span className={className}>{text}</span>
+                <span className={className} aria-label={tooltip}>{text}</span>
             );
         }
         return (null);
