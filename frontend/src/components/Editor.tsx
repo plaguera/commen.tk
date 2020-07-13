89cba94acb4f32129f2c4d68ee84961349547bc1
Pedro Lagüera <laguerapedro@gmail.com>
2020-07-13 22:59:08 +0100

    First Editor Overhaul

--- frontend/src/components/Editor.tsx
+++ frontend/src/components/Editor.tsx
@@ -1,15 +1,16 @@
 import * as React from 'react';
-import Markdown from 'react-markdown';
-
+import ReactHtmlParser from 'react-html-parser';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import Avatar from './Avatar';
 import { EditorProps, EditorState } from '../props';
 import DetailsMenu from './DetailsMenu';
+import agent from '../agent';
 import env from '../environment';
 
 class Editor extends React.Component<EditorProps, EditorState> {
 
-    private ref = React.createRef<HTMLTextAreaElement>();
+    private textarea = React.createRef<HTMLTextAreaElement>();
+    private commentBtn = React.createRef<HTMLButtonElement>();
 
     constructor(props: EditorProps) {
         super(props);
@@ -17,24 +18,44 @@ class Editor extends React.Component<EditorProps, EditorState> {
             text: ''
         }
         this.onComment = this.onComment.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
     handleClick(e: any) {
-        this.setState({ text: this.ref.current?.value || '' });
+        agent.GitHub
+            .markdown(this.textarea.current?.value || '')
+            .then(text => this.setState({ text: text || '' }));
     }
 
     onComment() {
-        this.setState({ text: this.ref.current?.value || '' }, () => {
+        this.setState({ text: this.textarea.current?.value || '' }, () => {
             this.props.onComment(this.state.text);
-            if (this.ref.current) this.ref.current.value = '';
+            if (this.textarea.current) this.textarea.current.value = '';
         });
     }
+
+    handleChange() {
+        let chars = this.textarea.current?.value.length || 0;
+        let disabled = this.commentBtn.current?.hasAttribute('disabled');
+        if (chars > 0 && disabled) this.commentBtn.current?.removeAttribute('disabled');
+        else if (chars === 0 && !disabled) this.commentBtn.current?.setAttribute('disabled', 'true');
+    }
+
+    /*
+
+    <a href='https://guides.github.com/features/mastering-markdown/'>
+                                            <span>
+                                                <svg className="octicon octicon-markdown v-align-bottom" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M14.85 3H1.15C.52 3 0 3.52 0 4.15v7.69C0 12.48.52 13 1.15 13h13.69c.64 0 1.15-.52 1.15-1.15v-7.7C16 3.52 15.48 3 14.85 3zM9 11H7V8L5.5 9.92 4 8v3H2V5h2l1.5 2L7 5h2v6zm2.99.5L9.5 8H11V5h2v3h1.5l-2.51 3.5z"></path></svg>
+                                            </span>
+                                        </a>
+
+                                        */
 
     render() {
         if (this.props.viewer) {
             return (
                 <div className='editor-wrapper'>
-                    <Avatar {...this.props.viewer}/>
+                    <Avatar {...this.props.viewer} />
                     <div className="editor arrow-box">
                         <Tabs forceRenderTabPanel={true}>
                             <TabList>
@@ -47,19 +68,27 @@ class Editor extends React.Component<EditorProps, EditorState> {
 
                             <TabPanel>
                                 <div className="editor-textarea" id="tab-editor">
-                                    <textarea placeholder="Leave a comment" id="textarea-comment" ref={this.ref}></textarea>
+                                    <textarea placeholder="Leave a comment" id="textarea-comment" ref={this.textarea} onChange={this.handleChange}></textarea>
+                                    <label>
+                                        <span>
+                                            <span>
+                                                Attach files by dragging &amp; dropping, selecting or pasting them.
+                                            </span>
+                                        </span>
+                                        
+                                    </label>
                                 </div>
                             </TabPanel>
                             <TabPanel>
                                 <div className="editor-preview" id="tab-preview">
                                     <div className="markdown-render">
-                                        <Markdown source={this.state.text} />
+                                        {ReactHtmlParser(this.state.text !== '' ? this.state.text : '<p>Nothing to preview</p>')}
                                     </div>
                                 </div>
                             </TabPanel>
                         </Tabs>
                         <div className="editor-buttons">
-                            <button className="btn btn-primary" id="btn-comment" onClick={this.onComment}>
+                            <button className="btn btn-primary" id="btn-comment" onClick={this.onComment} ref={this.commentBtn} disabled>
                                 Comment
                             </button>
                         </div>
